b5759f7cdb2b06e14da0381d81d172822c1be7dc
aaronho@uw.edu <aaronho@aaronh.local>
2023-07-19 11:00:15 -0700

    Added a checkbox input

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 function App() {
   const [input, setInput] = useState("")
   const [displayInput, setDisplayInput] = useState("")
+  const [checkbox, setCheckbox] = useState(false)
 
   const handleOnChange = (event) => {
     setInput(event.target.value);
@@ -15,6 +16,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <input data-testid="checkbox" type="checkbox" onChange={() => setCheckbox(!checkbox)} value={checkbox}/>
+      <label>{"This checkbox is now set to " + checkbox}</label>
+      <br />
       <input value={input} onChange={handleOnChange}></input>
       <button onClick={onClickButton}>Enter</button>
       <p>{displayInput}</p>
