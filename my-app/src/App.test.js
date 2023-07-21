de21525e2de768308dbab906495a025c0fc6c891
wphan-1925132 <wphan@uw.edu>
2023-07-20 17:51:31 -0700

    updated test

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  userEvent.click(screen.getByTestId("checkbox"));
+  expect(screen.getByTestId("checkbox")).toBeChecked();
+});
+
+test('testing for correct header', () => {
+  render(<App />);
+  screen.debug();
+  expect(screen.getByText("Hello World")).toBeInTheDocument();
 });
